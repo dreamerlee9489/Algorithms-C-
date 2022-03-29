9b9e98ae208a97bc1e42186b4821d57cad9392ea
Dreamer <dreamerlee9489@outlook.com>
2022-03-29 22:36:53 +0800

    Update Person.h

--- Season 1/Person.h
+++ Season 1/Person.h
@@ -13,14 +13,28 @@ class Person
 public:
     int _age = 0;
     std::string _name = "name";
+    Person &operator=(const Person &rhs);
+    Person &operator=(Person &&rhs) noexcept;
     Person() = default;
-    Person(int age, std::string name)
-    {
-        _age = age;
-        _name = name;
-    }
+    Person(int age, std::string name) : _age(age), _name(name) {}
+    Person(const Person &p) { *this = p; }
+    Person(Person &&p) noexcept { *this = std::move(p); }
     ~Person() { std::cout << "delete " << this << "[" << _age << ", " << _name + "]\n"; }
 };
+
+Person &Person::operator=(const Person &rhs)
+{
+    _age = rhs._age;
+    _name = rhs._name;
+    return *this;
+}
+
+Person &Person::operator=(Person &&rhs) noexcept
+{
+    _age = std::move(rhs._age);
+    _name = std::move((std::string)rhs._name);
+    return *this;
+}
 
 std::istream &operator>>(std::istream &in, Person &p) { return in >> p._age >> p._name; }
 std::ostream &operator<<(std::ostream &out, const Person &p) { return out << &p << "[" << p._age << ", " << p._name + "]\n"; }
