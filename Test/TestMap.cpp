24be0d62e888e61e74db4c2df955e1eb62b854df
Dreamer <dreamerlee9489@outlook.com>
2023-02-22 22:58:05 +0800

    Update

---
+++ Test/TestMap.cpp
@@ -0,0 +1,20 @@
+#include <map>
+#include <iostream>
+using namespace std;
+
+class Test
+{
+public:
+    int i = 0;
+    Test(int i) { this->i = i; }
+    ~Test() = default;
+};
+
+int main(int argc, char const *argv[])
+{
+    map<int, Test*> m;
+    m[1] = new Test(10);
+    cout << m[1]->i << ", " << m[2] << "\n";
+    return 0;
+}
+
